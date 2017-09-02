ba2fad8c01f1da35dcdd12ded6717e9afe53af3c
Justin Cullen <jcullen@alum.mit.edu>
2017-09-01 21:10:02 -0400

    Some generic stuff.

--- src/parsers/mod.rs
+++ src/parsers/mod.rs
@@ -1,9 +1,54 @@
+#![feature(associated_constants)]
 
 pub mod lisp;
 pub mod tree;
+use pest::{RuleType, Parser};
+use pest::iterators::Pair;
+use pest::inputs::{StringInput, Input};
+use std::str::FromStr;
+use std::marker::Sized;
 
-// trait RepParser: ::pest::Parser {
-//     type Rep;
-//     fn represent()
-    
-// }
+use errors::*;
+
+trait RepParser {
+    type Rule: RuleType;
+    type Rep;
+    const rule: Self::Rule;
+    fn represent(Pair<Self::Rule, StringInput>) -> Res<Self::Rep>;
+}
+
+trait Parseable {
+    type Err;
+    type Input: Input;
+    fn parsed<T>(&self) -> Result<T, Self::Err>
+        where T: FromParse;
+
+}
+
+impl<T: AsRef<str>> Parseable for T {
+    type Input = StringInput;
+    type Err = Error;
+    fn parsed<U: FromParse>(&self) -> Res<U> {
+        let s = self.as_ref();
+        let pairs = U::Parser::parse_str(U::rule, s);
+        match pairs {
+            Err(_) => return Err("Parsing failed.".into()),
+            Ok(mut pairs) => {
+                match pairs.next() {
+                    Some(data) => {
+                        U::represent(data)
+                    },
+                    None => Err("Nothing parsed.".into()),
+                }
+            }
+        }
+
+    }
+}
+
+trait FromParse: Sized {
+    type Rule: RuleType;
+    type Parser: Parser<Self::Rule>;
+    const rule: Self::Rule;
+    fn represent<I:Input>(Pair<Self::Rule, I>) -> Res<Self>;
+}
